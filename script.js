6e4892f4d4cfad96fe1ab31435d6f01839aa318a
Prajwal Katkar <prajwalkatkar17@gmail.com>
2026-01-26 19:04:50 +0530

    Add click event listeners for alerts

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector(".primary").addEventListener("click", () => {
+    alert("Resume will be uploaded soon 🚀");
+});
+
+document.querySelector(".secondary").addEventListener("click", () => {
+    alert("Projects section coming next 💻");
+});
