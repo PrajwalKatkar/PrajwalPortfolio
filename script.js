14043f88430b738aa188006663371fd75fec0a99
Prajwal Katkar <prajwalkatkar17@gmail.com>
2026-02-13 07:40:49 +0530

    Remove extra newline in script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.querySelector(".primary").addEventListener("click", () => {
     alert("Resume will be uploaded soon 🚀");
 });
-
 document.querySelector(".secondary").addEventListener("click", () => {
     alert("Projects section coming next 💻");
 });
